1e55be0de7e3ff6487423391bbcea9c5b84bc3c0
Xavier Mouy <xavier.mouy@nmfs.local>
2024-09-03 11:58:34 -0400

    corrected path

--- scripts/utils/correct_audiofiles_location.py
+++ scripts/utils/correct_audiofiles_location.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 
 detec_dir =r'\\nefscdata\PassiveAcoustics\DETECTOR_OUTPUT\PYTHON_MINKE\KETOS_v0.2\Raw\NEFSC_GOM\NEFSC_GOM_202102_MDR'
-new_audio_dir = r'/mnt/PassiveAcoustics_Soundfiles/PassiveAcoustics_Soundfiles/BOTTOM_MOUNTED/NEFSC_GOM/NEFSC_GOM_202102_MDR/5443_48kHz_UTC'
+new_audio_dir = r'/mnt/PassiveAcoustics_Soundfiles/BOTTOM_MOUNTED/NEFSC_GOM/NEFSC_GOM_202102_MDR/5443_48kHz_UTC'
 
 print('Starting to update audio files paths.')
 nc_files = list_files(detec_dir,suffix='.nc')
